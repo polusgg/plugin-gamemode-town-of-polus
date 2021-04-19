4d342b0e258eb584dc53cb4cc9da337033a93fa5
Rose Hall <rose@hall.ly>
2021-04-19 17:20:42 -0400

    game options please

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import { RoleAssignmentData } from "@polusgg/plugin-polusgg-api/src/services/roleManager/roleManagerService";
-import { Impostor } from "@polusgg/plugin-polusgg-api/src/baseRole/impostor/impostor";
 import { BaseMod } from "@polusgg/plugin-polusgg-api/src/baseMod/baseMod";
 import { PluginMetadata } from "@nodepolus/framework/src/api/plugin";
 import { LobbyInstance } from "@nodepolus/framework/src/api/lobby";
@@ -7,42 +6,51 @@ import { Jester } from "./src/roles/jester";
 import { Sheriff } from "./src/roles/sheriff";
 import { Snitch } from "./src/roles/snitch";
 import { AssetBundle } from "@polusgg/plugin-polusgg-api/src/assets";
+import { Services } from "@polusgg/plugin-polusgg-api/src/services";
+import { ServiceType } from "@polusgg/plugin-polusgg-api/src/types/enums";
+import { Engineer } from "./src/roles/engineer";
+import { Grenadier } from "./src/roles/grenadier";
+import { Morphling } from "./src/roles/morphling";
+import { Oracle } from "./src/roles/oracle";
+import { Phantom } from "./src/roles/phantom";
+import { SerialKiller } from "./src/roles/serialKiller";
+import { NumberValue } from "@polusgg/plugin-polusgg-api/src/packets/root/setGameOption";
 
 type TownOfPolusGameOptions = {
   /* Engineer */
-  engineerProbability: number;
-  engineerCooldown: number;
+  engineerProbability: NumberValue;
+  engineerCooldown: NumberValue;
 
   /* Grenadier */
-  grenadierProbability: number;
-  grenadierCooldown: number;
+  grenadierProbability: NumberValue;
+  grenadierCooldown: NumberValue;
 
   /* Jester */
-  jesterProbability: number;
+  jesterProbability: NumberValue;
 
   /* Morphling */
-  morphlingProbability: number;
-  morphlingCooldown: number;
+  morphlingProbability: NumberValue;
+  morphlingCooldown: NumberValue;
 
   /* Oracle */
-  oracleProbability: number;
-  oracleCooldown: number;
-  oracleAccuracy: number;
+  oracleProbability: NumberValue;
+  oracleCooldown: NumberValue;
+  oracleAccuracy: NumberValue;
 
   /* Phantom */
-  phantomProbability: number;
+  phantomProbability: NumberValue;
 
   /* Serial Killer */
-  serialKillerProbability: number;
-  serialKillerCooldown: number;
+  serialKillerProbability: NumberValue;
+  serialKillerCooldown: NumberValue;
 
   /* Sheriff */
-  sheriffProbability: number;
-  sheriffCooldown: number;
+  sheriffProbability: NumberValue;
+  sheriffCooldown: NumberValue;
 
   /* Snitch */
-  snitchProbability: number;
-  remainingTasks: number;
+  snitchProbability: NumberValue;
+  remainingTasks: NumberValue;
 };
 
 const pluginMetadata: PluginMetadata = {
@@ -70,25 +78,74 @@ export default class extends BaseMod {
     // todo set task strings for all impostor and neutral types
   }
 
-  getRoles(_lobby: LobbyInstance): RoleAssignmentData[] {
+  getRoles(lobby: LobbyInstance): RoleAssignmentData[] {
+    const gameOptions = Services.get(ServiceType.GameOptions).getGameOptions<TownOfPolusGameOptions>(lobby);
+
     return [
       {
+        role: Engineer,
+        playerCount: this.resolveOptionPercent(gameOptions.getOption("engineerProbability").getValue().value),
+      }, {
+        role: Grenadier,
+        playerCount: this.resolveOptionPercent(gameOptions.getOption("grenadierProbability").getValue().value),
+      }, {
         role: Jester,
-        playerCount: 1,
+        playerCount: this.resolveOptionPercent(gameOptions.getOption("jesterProbability").getValue().value),
+      }, {
+        role: Morphling,
+        playerCount: this.resolveOptionPercent(gameOptions.getOption("morphlingProbability").getValue().value),
+      }, {
+        role: Oracle,
+        playerCount: this.resolveOptionPercent(gameOptions.getOption("oracleProbability").getValue().value),
+      }, {
+        role: Phantom,
+        playerCount: this.resolveOptionPercent(gameOptions.getOption("phantomProbability").getValue().value),
+      }, {
+        role: SerialKiller,
+        playerCount: this.resolveOptionPercent(gameOptions.getOption("serialKillerProbability").getValue().value),
       }, {
         role: Sheriff,
-        playerCount: 1,
+        playerCount: this.resolveOptionPercent(gameOptions.getOption("sheriffProbability").getValue().value),
       }, {
         role: Snitch,
-        playerCount: 1,
-      }, {
-        role: Impostor,
-        playerCount: 1,
+        playerCount: this.resolveOptionPercent(gameOptions.getOption("snitchProbability").getValue().value),
       },
     ];
   }
 
   getEnabled(): boolean {
     return true;
+  }
+
+  onEnable(lobby: LobbyInstance): void {
+    const gameOptions = Services.get(ServiceType.GameOptions).getGameOptions<TownOfPolusGameOptions>(lobby);
+
+    gameOptions.createOption("engineerProbability", new NumberValue(50, 10, 0, 100, false, "{0}%"));
+    gameOptions.createOption("grenadierProbability", new NumberValue(50, 10, 0, 100, false, "{0}%"));
+    gameOptions.createOption("jesterProbability", new NumberValue(50, 10, 0, 100, false, "{0}%"));
+    gameOptions.createOption("morphlingProbability", new NumberValue(50, 10, 0, 100, false, "{0}%"));
+    gameOptions.createOption("oracleProbability", new NumberValue(50, 10, 0, 100, false, "{0}%"));
+    gameOptions.createOption("phantomProbability", new NumberValue(50, 10, 0, 100, false, "{0}%"));
+    gameOptions.createOption("serialKillerProbability", new NumberValue(50, 10, 0, 100, false, "{0}%"));
+    gameOptions.createOption("sheriffProbability", new NumberValue(50, 10, 0, 100, false, "{0}%"));
+    gameOptions.createOption("snitchProbability", new NumberValue(50, 10, 0, 100, false, "{0}%"));
+  }
+
+  onDisable(lobby: LobbyInstance): void {
+    const gameOptions = Services.get(ServiceType.GameOptions).getGameOptions<TownOfPolusGameOptions>(lobby);
+
+    gameOptions.deleteOption("engineerProbability");
+    gameOptions.deleteOption("grenadierProbability");
+    gameOptions.deleteOption("jesterProbability");
+    gameOptions.deleteOption("morphlingProbability");
+    gameOptions.deleteOption("oracleProbability");
+    gameOptions.deleteOption("phantomProbability");
+    gameOptions.deleteOption("serialKillerProbability");
+    gameOptions.deleteOption("sheriffProbability");
+    gameOptions.deleteOption("snitchProbability");
+  }
+
+  private resolveOptionPercent(percent: number): number {
+    return Math.round(percent / 100);
   }
 }
