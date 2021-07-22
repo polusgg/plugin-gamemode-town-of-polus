d47cab69b7750918da3298799c3512483e9a05e1
Dumb Player2 <84459844+dumbplayer2@users.noreply.github.com>
2021-07-23 00:07:58 +0200

    Update jester.ts
    Jester dead TaskText

--- src/roles/jester.ts
+++ src/roles/jester.ts
@@ -1,7 +1,7 @@
 import { StartGameScreenData } from "@polusgg/plugin-polusgg-api/src/services/roleManager/roleManagerService";
 import { BaseManager } from "@polusgg/plugin-polusgg-api/src/baseManager/baseManager";
 import { RoleAlignment, RoleMetadata } from "@polusgg/plugin-polusgg-api/src/baseRole/baseRole";
-import { ServiceType } from "@polusgg/plugin-polusgg-api/src/types/enums";
+import { Location, ServiceType } from "@polusgg/plugin-polusgg-api/src/types/enums";
 import { PlayerInstance } from "@nodepolus/framework/src/api/player";
 import { BaseRole } from "@polusgg/plugin-polusgg-api/src/baseRole";
 import { Services } from "@polusgg/plugin-polusgg-api/src/services";
@@ -13,6 +13,9 @@ export class JesterManager extends BaseManager {
   getId(): string { return "jester" }
   getTypeName(): string { return "Jester" }
 }
+
+const JESTER_DEAD_STRING = `<color=#ff8cee>Role: Jester</color>
+<color=#ff1919>You're dead.</color>`;
 
 export class Jester extends BaseRole {
   protected metadata: RoleMetadata = {
@@ -92,4 +95,9 @@ export class Jester extends BaseRole {
     return `<color=#ff8cee>Role: Jester
 Trick everyone into voting you out.</color>`;
   }
+
+this.catch("player.murdered", e => e.getPlayer()).execute(event => {
+Services.get(ServiceType.Hud).setHudString(event.getPlayer(), Location.TaskText, JESTER_DEAD_STRING);
+});
+
 }
