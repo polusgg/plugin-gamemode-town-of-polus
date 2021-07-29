b83c3edf347bae204b550109e72582a4695df2c1
Rose Hall <rose@hall.ly>
2021-07-29 14:32:00 -0400

    removed color tags

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,17 @@
 export enum TownOfPolusGameOptionNames {
-  EngineerProbability = "<size=150%><sprite index=7></size> <color=#F8BF14>Engineer</color> <color=#0b6e99></color>",
+  EngineerProbability = "<size=150%><sprite index=7></size> <color=#F8BF14>Engineer</color>",
 
-  SwooperProbability = "<size=150%></size> <color=#969696>Swooper</color> <color=#e03e3e></color>",
+  SwooperProbability = "<size=150%></size> <color=#969696>Swooper</color>",
   SwooperCooldown = "<color=#969696>Swooper</color> Cooldown",
   SwooperAbilityDuration = "<color=#969696>Swooper</color> Ability Duration",
 
-  PoisonerProbability = "<size=150%></size> <color=#1b963c>Poisoner</color> <color=#e03e3e></color>",
+  PoisonerProbability = "<size=150%></size> <color=#1b963c>Poisoner</color>",
   PoisonerCooldown = "<color=#1b963c>Poisoner</color> Cooldown",
   PoisonerRange = "<color=#1b963c>Poisoner</color> Range",
   //PoisonerTargets = "<color=#1b963c>Poisoner</color> Can Poison Multiple Targets in Range",
   PoisonerPoisonDuration = "<color=#1b963c>Poison</color> Kills After",
 
-  GrenadierProbability = "<size=150%><sprite index=5></size> <color=#728F3D>Grenadier</color> <color=#e03e3e></color>",
+  GrenadierProbability = "<size=150%><sprite index=5></size> <color=#728F3D>Grenadier</color>",
   GrenadierCooldown = "<color=#728F3D>Grenadier</color> Cooldown",
   // GrenadierRange = "<color=#728F3D>Grenadier</color> Range",
   GrenadierBlindness = "<color=#728F3D>Grenadier</color> Blindness",
@@ -21,7 +21,7 @@ export enum TownOfPolusGameOptionNames {
   // MorphlingProbability = "Morphling Probability",
   // MorphlingCooldown = "Morphling Cooldown",
 
-  OracleProbability = "<size=150%><sprite index=8></size> <color=#2c4cc9>Oracle</color> <color=#0b6e99></color>",
+  OracleProbability = "<size=150%><sprite index=8></size> <color=#2c4cc9>Oracle</color>",
   OracleCooldown = "<color=#2c4cc9>Oracle</color> Cooldown",
   OracleAccuracy = "<color=#2c4cc9>Oracle</color> Accuracy",
 
@@ -34,13 +34,13 @@ export enum TownOfPolusGameOptionNames {
   SerialKillerCooldown = "<color=#ff547c>Serial Killer</color> Cooldown",
   SerialKillerMinPlayers = "<color=#ff547c>Serial Killer</color> Minimum Players",
 
-  SheriffProbability = "<size=150%><sprite index=12></size> <color=#c49645>Sheriff</color> <color=#0b6e99></color>",
+  SheriffProbability = "<size=150%><sprite index=12></size> <color=#c49645>Sheriff</color>",
   SheriffCooldown = "<color=#c49645>Sheriff</color> Cooldown",
 
-  SnitchProbability = "<size=150%><sprite index=11></size> <color=#00ffdd>Snitch</color> <color=#0b6e99></color>",
+  SnitchProbability = "<size=150%><sprite index=11></size> <color=#00ffdd>Snitch</color>",
   SnitchRemainingTasks = "<color=#00ffdd>Snitch</color> Remaining Tasks",
 
-  LocksmithProbability = "<size=150%><sprite index=14></size> <color=#3d85c6>Locksmith</color> <color=#0b6e99></color>",
+  LocksmithProbability = "<size=150%><sprite index=14></size> <color=#3d85c6>Locksmith</color>",
   LocksmithCooldown = "<color=#3d85c6>Locksmith</color> Cooldown",
   LocksmithUses = "<color=#3d85c6>Locksmith</color> Uses",
   LocksmithRange = "<color=#3d85c6>Locksmith</color> Range",
